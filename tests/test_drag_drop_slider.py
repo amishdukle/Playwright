d10f702be30fad6ce5c4bc4366405b871f4dae07
amishdukle <117905498+amishdukle@users.noreply.github.com>
2025-03-01 21:04:54 +0530

    Update test_drag_drop_slider.py

--- tests/test_drag_drop_slider.py
+++ tests/test_drag_drop_slider.py
@@ -3,11 +3,11 @@ from config import URL, MESSAGE
 
 def test_drag_drop_slider():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(URL)
         page.click("text=Drag & Drop Sliders")
         slider = page.locator("input[value='15']")
         slider.fill("95")
-        expect(page.locator("#range")).to_have_text("95")
+        expect(page.locator("#rangeSuccess")).to_have_text("95")
         browser.close()
