8f5338288290201f445272b8998306f232cde6fe
amishdukle <117905498+amishdukle@users.noreply.github.com>
2025-03-01 21:08:56 +0530

    Update test_drag_drop_slider.py

--- tests/test_drag_drop_slider.py
+++ tests/test_drag_drop_slider.py
@@ -3,7 +3,7 @@ from config import URL, MESSAGE
 
 def test_drag_drop_slider():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(URL)
         page.click("text=Drag & Drop Sliders")
