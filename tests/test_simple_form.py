c5ed8c5693351d586b03689b0d55823a3479eeae
amishdukle <117905498+amishdukle@users.noreply.github.com>
2025-03-01 21:05:15 +0530

    Update test_simple_form.py

--- tests/test_simple_form.py
+++ tests/test_simple_form.py
@@ -1,9 +1,9 @@
 from playwright.sync_api import sync_playwright, expect
-from ..config import URL, MESSAGE
+from config import URL, MESSAGE
 
 def test_simple_form():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(URL)
         page.click("text=Simple Form Demo")
