c80521e7af41b28d8e2031993e2e84c8ccab1939
amishdukle <117905498+amishdukle@users.noreply.github.com>
2025-03-01 18:40:50 +0530

    Delete playwright.config.py

--- playwright.config.py
+++
@@ -1,6 +0,0 @@
-# playwright.config.py
-import os
-from playwright.sync_api import sync_playwright, expect
-
-URL = 'https://www.lambdatest.com/selenium-playground'
-MESSAGE = 'Welcome to LambdaTest'
